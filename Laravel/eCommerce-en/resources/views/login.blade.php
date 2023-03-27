803d32ec6444f236658a7fa1787a1c066fa50309
Turgay Şahin <trueguytr@gmail.com>
2023-03-27 15:12:32 +0300

    eCommerce-en-Laravel-MezuniyetProjesi

--- Laravel/eCommerce-en/resources/views/login.blade.php
+++ Laravel/eCommerce-en/resources/views/login.blade.php
@@ -3,20 +3,17 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 mx-auto ">
-            <form>
+            <form action="login" method="POST">
                 <div class="form-group">
+                  @csrf 
                   <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                 </div>
-                <div class="form-group form-check">
-                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Login</button>
               </form>
         </div>
     </div>
